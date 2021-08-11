a3558b8e32dccfa706824369b3c62388a2598b96
郭耀展 <yaozhan.guo@zhenai.com>
2021-08-11 15:18:30 +0800

    48. Rotate Image(https://leetcode.com/problems/rotate-image/)

---
+++ src/main/java/name/guolanren/_1to100/_41to50/p46/RotateImage.java
@@ -0,0 +1,33 @@
+package name.guolanren._1to100._41to50.p46;
+
+/**
+ * @link https://leetcode.com/problems/rotate-image/
+ * @author guolanren
+ */
+public class RotateImage {
+
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n / 2; i++) {
+            for (int j = i; j < n - i - 1; j++) {
+                int replaceI = i;
+                int replaceJ = j;
+                int nextReplaceI = replaceJ;
+                int nextReplaceJ = n - 1 - replaceI;
+                int temp = matrix[replaceI][replaceJ];
+                do {
+                    replaceI = nextReplaceI;
+                    replaceJ = nextReplaceJ;
+
+                    int replace = temp;
+                    temp = matrix[nextReplaceI][nextReplaceJ];
+                    matrix[nextReplaceI][nextReplaceJ] = replace;
+
+                    nextReplaceI = replaceJ;
+                    nextReplaceJ = n - 1 - replaceI;
+                } while (replaceI != i || replaceJ != j);
+            }
+        }
+    }
+
+}
---
+++ src/test/java/name/guolanren/_1to100/_41to50/p46/RotateImageTest.java
@@ -0,0 +1,27 @@
+package name.guolanren._1to100._41to50.p46;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class RotateImageTest {
+
+    private RotateImage rotateImage;
+
+    @Before
+    public void before() {
+        rotateImage = new RotateImage();
+    }
+
+    @Test
+    public void test() {
+        int[][] matrix = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        rotateImage.rotate(matrix);
+        System.out.println(Arrays.deepToString(matrix));
+
+        int[][] matrix2 = new int[][]{{5, 1, 9, 11}, {2, 4, 8, 10}, {13, 3, 6, 7}, {15, 14, 12, 16}};
+        rotateImage.rotate(matrix2);
+        System.out.println(Arrays.deepToString(matrix2));
+    }
+}
