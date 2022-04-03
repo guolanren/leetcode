68553ccf6b7eef814f47cb1644436a8d85c4bb0c
郭耀展 <lanrenguo@gmail.com>
2022-04-03 08:55:55 +0800

    15. 3Sum(双指针)

--- src/main/java/name/guolanren/_1to100/_11to20/p15/_3Sum.java
+++ src/main/java/name/guolanren/_1to100/_11to20/p15/_3Sum.java
@@ -9,36 +9,44 @@ import java.util.*;
 public class _3Sum {
 
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
+        List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
-        Map<Integer, Integer> map = new HashMap<>(nums.length);
-
-        for (int i = 0; i < nums.length; i++) {
-            map.put(nums[i], i);
+        if (nums.length == 0 || nums[nums.length - 1] < 0) {
+            return res;
         }
 
         for (int i = 0; i < nums.length - 2; i++) {
-            if (nums[i] > 0) {
+            if (i != 0 && nums[i] == nums[i - 1]) {
+                continue;
+            } else if (nums[i] > 0) {
                 break;
             }
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
+            int j = i + 1;
+            int k = nums.length - 1;
+            int target = 0 - nums[i];
+
+            while (j < k) {
+                if (nums[j] + nums[k] < target) {
+                    do {
+                        j++;
+                    } while (j < k && nums[j] == nums[j - 1]);
+                } else if (nums[j] + nums[k] > target) {
+                    do {
+                        k--;
+                    } while (j < k && nums[k] == nums[k + 1]);
+                } else {
+                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    do {
+                        j++;
+                    } while (j < k && nums[j] == nums[j - 1]);
+                    do {
+                        k--;
+                    } while (j < k && nums[k] == nums[k + 1]);
+                }
             }
-            for (int j = i + 1; j < nums.length - 1; j++) {
-                int _2Sum = nums[i] + nums[j];
-                if (_2Sum > 0) {
-                    break;
-                }
-                if (j > i + 1 && nums[j] == nums[j - 1]) {
-                    continue;
-                }
-                Integer value = map.get(0 - _2Sum);
-                if (value != null && j < value) {
-                    result.add(Arrays.asList(nums[i], nums[j], nums[value]));
-                }
-            }
+
         }
-        return result;
+        return res;
     }
 
 }
