389454dac9460bfd9849d716bddcee640597e6f7
郭耀展 <yaozhan.guo@zhenai.com>
2022-04-18 17:56:57 +0800

    104. Maximum Depth of Binary Tree（递归）

--- src/main/java/name/guolanren/_101to200/_101to110/p104/MaximumDepthOfBinaryTree.java
+++ src/main/java/name/guolanren/_101to200/_101to110/p104/MaximumDepthOfBinaryTree.java
@@ -1,7 +1,5 @@
 package name.guolanren._101to200._101to110.p104;
 
-import java.util.LinkedList;
-
 /**
  * @link https://leetcode.com/problems/maximum-depth-of-binary-tree/
  * @author guolanren
@@ -12,27 +10,7 @@ public class MaximumDepthOfBinaryTree {
         if (root == null) {
             return 0;
         }
-        LinkedList<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
-        return maxDepth(queue);
-    }
-
-    private int maxDepth(LinkedList<TreeNode> queue) {
-        int depth = 0;
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            for (int i = 0; i < size; i++) {
-                TreeNode node = queue.poll();
-                if (node.left != null) {
-                    queue.offer(node.left);
-                }
-                if (node.right != null) {
-                    queue.offer(node.right);
-                }
-            }
-            depth++;
-        }
-        return depth;
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
     }
 
 }
