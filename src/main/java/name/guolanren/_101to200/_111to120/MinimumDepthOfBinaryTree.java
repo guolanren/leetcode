43a2cc5bc50777191408e720e678204affabcaeb
郭耀展 <yaozhan.guo@zhenai.com>
2022-04-19 09:52:43 +0800

    111. Minimum Depth of Binary Tree(https://leetcode.com/problems/minimum-depth-of-binary-tree/)

---
+++ src/main/java/name/guolanren/_101to200/_111to120/MinimumDepthOfBinaryTree.java
@@ -0,0 +1,55 @@
+package name.guolanren._101to200._111to120;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @link https://leetcode.com/problems/minimum-depth-of-binary-tree/
+ * @author guolanren
+ */
+public class MinimumDepthOfBinaryTree {
+
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        return minDepth(queue);
+    }
+
+    private int minDepth(Queue<TreeNode> queue) {
+        int minDepth = 1;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (node.left == null && node.right == null) {
+                    return minDepth;
+                }
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            minDepth++;
+        }
+        return minDepth;
+    }
+
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
---
+++ src/test/java/name/guolanren/_101to200/_111to120/MinimumDepthOfBinaryTreeTest.java
@@ -0,0 +1,19 @@
+package name.guolanren._101to200._111to120;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MinimumDepthOfBinaryTreeTest {
+
+    private MinimumDepthOfBinaryTree minimumDepthOfBinaryTree;
+
+    @Before
+    public void before() {
+        minimumDepthOfBinaryTree = new MinimumDepthOfBinaryTree();
+    }
+
+    @Test
+    public void test() {
+    }
+
+}
