bd6baa366a6acf682167f2fbcb2b9ddd0cfa1123
郭耀展 <yaozhan.guo@zhenai.com>
2022-04-29 14:46:24 +0800

    121. Best Time to Buy and Sell Stock（精简）

--- src/main/java/name/guolanren/_101to200/_121to130/p121/BestTimeToBuyAndSellStock.java
+++ src/main/java/name/guolanren/_101to200/_121to130/p121/BestTimeToBuyAndSellStock.java
@@ -10,17 +10,13 @@ public class BestTimeToBuyAndSellStock {
      * 都去掉
      */
     public int maxProfit(int[] prices) {
-        int max = 0;
         int has = - prices[0];
         int hasNot = 0;
         for (int i = 1; i < prices.length; i++) {
             hasNot = Math.max(hasNot, has + prices[i]);
             has = Math.max(has, - prices[i]);
-
-            max = Math.max(hasNot, max);
         }
-
-        return max;
+        return hasNot;
     }
 
     /**
@@ -29,15 +25,11 @@ public class BestTimeToBuyAndSellStock {
     public int maxProfit2(int[] prices) {
         int[][] dp = new int[prices.length][2];
         dp[0][1] = 0 - prices[0];
-        int max = 0;
         for (int i = 1; i < prices.length; i++) {
             dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
             dp[i][1] = Math.max(dp[i - 1][1], - prices[i]);
-
-            max = Math.max(dp[i][0], max);
         }
-
-        return max;
+        return dp[prices.length - 1][0];
     }
 
     /**
@@ -46,14 +38,10 @@ public class BestTimeToBuyAndSellStock {
     public int maxProfit3(int[] prices) {
         int[][][] dp = new int[prices.length][2][2];
         dp[0][1][1] = 0 - prices[0];
-        int max = 0;
         for (int i = 1; i < prices.length; i++) {
             dp[i][0][1] = Math.max(dp[i - 1][0][1], dp[i - 1][1][1] + prices[i]);
             dp[i][1][1] = Math.max(dp[i - 1][1][1], dp[i - 1][0][0] - prices[i]);
-
-            max = Math.max(dp[i][0][1], max);
         }
-
-        return max;
+        return dp[prices.length - 1][0][1];
     }
 }
