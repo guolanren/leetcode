6a511b6f7b2bfd8ed8ea5b0b8f74cc331f37421b
郭耀展 <yaozhan.guo@zhenai.com>
2022-07-26 13:00:43 +0800

    3. Longest Substring Without Repeating Characters(新方法)

--- src/main/java/name/guolanren/_1to100/_1to10/p3/LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/name/guolanren/_1to100/_1to10/p3/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,6 +1,8 @@
 package name.guolanren._1to100._1to10.p3;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -10,25 +12,46 @@ import java.util.Set;
 public class LongestSubstringWithoutRepeatingCharacters {
 
     public int lengthOfLongestSubstring(String s) {
-        int maxLength = 0;
-        int byeIndex = 0;
-        // 乱序的候选最长字符串
-        Set<Character> candidate = new HashSet<>();
-        for (int i = 0; i < s.length(); i++) {
-            // 已经追不上啦，放弃吧
-            if (s.length() - i + candidate.size() <= maxLength) {
+        int length = s.length();
+        int max = 0;
+        int from = 0;
+        Map<Character, Integer> record = new HashMap<>();
+        for (int i = 0; i < length; i++) {
+            if (length - from <= max) {
                 break;
             }
-            char c = s.charAt(i);
-            // 遇到重复的，按顺序删除，直到跟相同的字符说 bye 为止
-            while (candidate.contains(c)) {
-                char bye = s.charAt(byeIndex++);
-                candidate.remove(bye);
+            Character c = s.charAt(i);
+            Integer idx = record.get(c);
+            if (idx == null || idx < from) {
+                max = Math.max(max, i - from + 1);
+            } else {
+                from = idx + 1;
             }
-            candidate.add(c);
-            maxLength = Integer.max(maxLength, candidate.size());
+            record.put(c, i);
         }
-        return maxLength;
+        return max;
     }
+
+//    public int lengthOfLongestSubstring(String s) {
+//        int maxLength = 0;
+//        int byeIndex = 0;
+//        // 乱序的候选最长字符串
+//        Set<Character> candidate = new HashSet<>();
+//        for (int i = 0; i < s.length(); i++) {
+//            // 已经追不上啦，放弃吧
+//            if (s.length() - i + candidate.size() <= maxLength) {
+//                break;
+//            }
+//            char c = s.charAt(i);
+//            // 遇到重复的，按顺序删除，直到跟相同的字符说 bye 为止
+//            while (candidate.contains(c)) {
+//                char bye = s.charAt(byeIndex++);
+//                candidate.remove(bye);
+//            }
+//            candidate.add(c);
+//            maxLength = Integer.max(maxLength, candidate.size());
+//        }
+//        return maxLength;
+//    }
 
 }
