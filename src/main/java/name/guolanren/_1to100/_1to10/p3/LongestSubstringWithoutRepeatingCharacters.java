e5776d20da281fc7f55ffa7f3298584594a38b4c
郭耀展 <lanrenguo@gmail.com>
2023-01-04 23:56:49 +0800

    3. Longest Substring Without Repeating Characters(https://leetcode.com/problems/longest-substring-without-repeating-characters/)

--- src/main/java/name/guolanren/_1to100/_1to10/p3/LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/name/guolanren/_1to100/_1to10/p3/LongestSubstringWithoutRepeatingCharacters.java
@@ -12,46 +12,19 @@ import java.util.Set;
 public class LongestSubstringWithoutRepeatingCharacters {
 
     public int lengthOfLongestSubstring(String s) {
-        int length = s.length();
         int max = 0;
-        int from = 0;
         Map<Character, Integer> record = new HashMap<>();
-        for (int i = 0; i < length; i++) {
-            if (length - from <= max) {
-                break;
-            }
+        for (int i = 0, j = 0; i < s.length(); i++) {
             Character c = s.charAt(i);
             Integer idx = record.get(c);
-            if (idx == null || idx < from) {
-                max = Math.max(max, i - from + 1);
+            if (idx == null || idx < j) {
+                max = Math.max(max, i - j + 1);
             } else {
-                from = idx + 1;
+                j = idx + 1;
             }
             record.put(c, i);
         }
         return max;
     }
-
-//    public int lengthOfLongestSubstring(String s) {
-//        int maxLength = 0;
-//        int byeIndex = 0;
-//        // 乱序的候选最长字符串
-//        Set<Character> candidate = new HashSet<>();
-//        for (int i = 0; i < s.length(); i++) {
-//            // 已经追不上啦，放弃吧
-//            if (s.length() - i + candidate.size() <= maxLength) {
-//                break;
-//            }
-//            char c = s.charAt(i);
-//            // 遇到重复的，按顺序删除，直到跟相同的字符说 bye 为止
-//            while (candidate.contains(c)) {
-//                char bye = s.charAt(byeIndex++);
-//                candidate.remove(bye);
-//            }
-//            candidate.add(c);
-//            maxLength = Integer.max(maxLength, candidate.size());
-//        }
-//        return maxLength;
-//    }
 
 }
